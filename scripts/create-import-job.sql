30db71d7d4a890d1236cfeb5df9bf446e949d2a9
YuveerK <yuveerkal01@gmail.com>
2025-06-15 00:37:14 +0200

    Automate Job Creation

---
+++ scripts/create-import-job.sql
@@ -0,0 +1,60 @@
+USE msdb;
+GO
+
+DECLARE @jobName NVARCHAR(100) = N'import data';
+DECLARE @jobId UNIQUEIDENTIFIER;
+DECLARE @shouldReplace BIT = 1; -- Change to 0 if you don't want auto-replacement
+
+-- Check if the job exists
+SELECT @jobId = job_id FROM msdb.dbo.sysjobs WHERE name = @jobName;
+
+-- Drop job if exists and replacement is enabled
+IF @jobId IS NOT NULL AND @shouldReplace = 1
+BEGIN
+    EXEC msdb.dbo.sp_delete_job @job_name = @jobName;
+    SET @jobId = NULL;
+END
+
+-- Only create if it doesn't exist
+IF @jobId IS NULL
+BEGIN
+    -- Create job
+    EXEC msdb.dbo.sp_add_job 
+        @job_name = @jobName,
+        @enabled = 1,
+        @description = N'Runs the Import Timesheet Data Audit SSIS package.',
+        @owner_login_name = N'YUVEER\yuvee',
+        @job_id = @jobId OUTPUT;
+
+    -- Add job step
+    EXEC msdb.dbo.sp_add_jobstep 
+        @job_id = @jobId,
+        @step_name = N'Import timesheet files',
+        @subsystem = N'SSIS',
+        @command = N'/ISSERVER "\"\SSISDB\Timesheet Imports\Normalized_SSIS_Project\Import Timesheet Data Audit.dtsx\"" /SERVER YUVEER /Par "\"$ServerOption::LOGGING_LEVEL(Int16)\"";1 /Par "\"$ServerOption::SYNCHRONIZED(Boolean)\"";True /CALLERINFO SQLAGENT /REPORTING E',
+        @database_name = N'master',
+        @on_success_action = 1,
+        @on_fail_action = 2;
+
+    -- Add schedule
+    EXEC msdb.dbo.sp_add_schedule 
+        @schedule_name = N'Run every minute',
+        @enabled = 1,
+        @freq_type = 4,  -- Daily
+        @freq_interval = 1,
+        @freq_subday_type = 4,  -- Minutes
+        @freq_subday_interval = 1,
+        @active_start_date = CONVERT(INT, CONVERT(VARCHAR, GETDATE(), 112)),  -- Today
+        @active_start_time = 0;
+
+    -- Attach schedule
+    EXEC msdb.dbo.sp_attach_schedule 
+        @job_name = @jobName,
+        @schedule_name = N'Run every minute';
+
+    -- Register the job to run on local server
+    EXEC msdb.dbo.sp_add_jobserver 
+        @job_name = @jobName,
+        @server_name = N'(local)';
+END
+GO
